9386fceab3a2341770b28bd4d94158f2af655830
Hideki Matsuoka <matsuoka.hide@gmail.com>
2018-09-24 00:34:36 +0900

    Initial Commit

---
+++ TechBookFestivalAPIClientPlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechBookFestivalAPIClientPlayground
+//
+//  Created by Hideki Matsuoka on 2018/09/24.
+//  Copyright © 2018年 matsuokah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
